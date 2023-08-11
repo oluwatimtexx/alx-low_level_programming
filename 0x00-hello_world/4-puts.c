621e238cdbe0343c22e25d4c92e789773e10aa98
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-11 10:24:07 -0700

    first c code using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the string using function puts
+ * Description: Using the main function to output strings
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+
+}
