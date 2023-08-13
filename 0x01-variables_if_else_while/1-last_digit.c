47f0505539e3f7947d4a7f63e91ee1d105c846cf
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-13 15:26:26 -0700

    Learning to use the IF statement and debug

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,36 +1,23 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 /**
- *  main - Entry point
- *  Return: 0
+ * main - Entry point
+ * Return: 0
  */
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-	int last_digit;
-
+	int last;
+	/* your code goes there */
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	/* your code goes there */
-	if (n > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	}
-	else
-	{
-		if (n == 0)
-		{
-			printf("Last digit of %d is %d and is 0\n", n, last_digit);
-		}
-		else
-		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-		}
-	}
+	last = n % 10;
+	if (last > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %i is %i and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	return (0);
 }
