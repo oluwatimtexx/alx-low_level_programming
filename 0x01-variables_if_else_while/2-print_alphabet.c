8bbe32306498beead8749c06ee69ba018f1aa633
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-14 15:02:31 -0700

    using putchar to output  small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - Entry point
+ *  Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
