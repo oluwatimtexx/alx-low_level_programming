74e2e9bf13cabbc36007f68a39d26759e376be59
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-14 15:36:44 -0700

    Using putchar to print both small and capital letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter = 'a';
+	int i = 0;
+/* your code goes there */
+	while (i < 26)
+	{
+		putchar(letter);
+		letter++;
+		i++;
+	}
+	letter = 'A';
+
+	while (i < 26)
+	{
+		putchar(letter);
+		letter++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
