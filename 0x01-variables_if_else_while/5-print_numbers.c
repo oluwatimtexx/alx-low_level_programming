89cb6f2d48fcd91ece52379f89bf8c450eed346b
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-14 15:58:51 -0700

    using for loop to print out numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+	/* your code goes there */
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%i", num);
+	}
+	printf("\n");
+	return (0);
+}
