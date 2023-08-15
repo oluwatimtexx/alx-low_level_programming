41068128dcc362bb42ab08e76769e2bc0f50b1ba
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-15 12:11:41 -0700

    Printing the absolute value of an integer using if condition

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @num: Used to recieve number into the function
+ * Return: num
+ */
+
+int _abs(int num);
+{
+	if (num > 0 || num == 0)
+	{
+		return (num);
+	}
+	else
+	{
+		return (-num);
+	}
+}
