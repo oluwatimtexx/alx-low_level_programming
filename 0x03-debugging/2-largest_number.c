8e652b090253f71e16410ec982a0b9e8a0a99c34
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-17 05:24:14 -0700

    correcting the code to print out the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,8 +16,10 @@ int largest_number(int a, int b, int c)
 		largest = a;
 	else if (b > a && b > c)
 		largest = b;
-	else
+	else if (c > b)
 		largest = c;
+	else
+		largest = b;
 
 	return (largest);
 }
