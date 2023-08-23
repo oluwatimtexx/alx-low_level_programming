b356381baf96501c30787598c38f02641afd575f
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-22 19:12:00 -0700

    counting the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Entry to function to return length of a string
+ * @s: parameter to count the string
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[i])
+		i++
+
+	return (i);
+}
