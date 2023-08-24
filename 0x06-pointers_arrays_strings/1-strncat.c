95a0ab2d113d71cf61f565e3ac64e3ca4c702b0e
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-24 02:08:54 -0700

    contatenate two strings based on n number of integer

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - Entry point to funct that concatinates 2 strings
+ * @dest: first pointer to a string
+ * @src: second pointer to a string
+ * @n: number of characters to concatenate
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count2, count3;
+
+	for (count2 = 0; dest[count2] != '\0'; count2++)
+		;
+
+	while (count3 < n && src[count3] != '\0')
+	{
+		dest[count2 + count3] = src[count3];
+		count3++
+	}
+	dest[count2 + count3] = '\0';
+
+	return (dest);
+}
