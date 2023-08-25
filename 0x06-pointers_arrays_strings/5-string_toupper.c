6fb40ecc0c8160df908d9581795199cfc841a8f9
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-24 19:46:54 -0700

    function changes lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,8 +12,7 @@ char *string_toupper(char *a)
 	for (count = 0; a[count] != '\0'; count++)
 	{
 		if (a[count] >= 97 && a[count] <= 122)
-			a[count] =- 32;
-		count++;
+			a[count] = a[count] - 32;
 	}
 	return (a);
 }
