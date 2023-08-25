633cc535a9f9136b25e0c0796fa20a3a20e0f937
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-24 19:37:25 -0700

    function changes lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper -Entry point ot function that changes lowercase to uppercase
+ * @a: pointer
+ * Return: uppercase strings
+ */
+
+char *string_toupper(char *a)
+{
+	int count;
+
+	for (count = 0; a[count] = '\0'; count++)
+	{
+		if (a[count] >= 97 && a[count] <= 122)
+			a[count] =- 32;
+		count++;
+	}
+	return (a);
+}
