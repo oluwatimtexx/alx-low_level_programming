b07294dc37480cd6765780c5d1636f261edf81fb
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-24 20:21:25 -0700

    encoding a message from a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.c"
+/**
+ * leet - Entry point to enccoder
+ * @a: pointer to string
+ * Return: encryption
+ */
+
+char *leet(char *a)
+{
+	int c1, c2;
+	char message[] = "aAeEoOtTlL";
+	char encode[] = "4433007711";
+
+	for (c1 = 0; a[c1] != '\0'; c1++)
+	{
+		for (c2 = 0; message[c2] !='\0'; c2++)
+		{
+			if (a[c1] == message[c2])
+			{
+				a[c1] = encode[c2];
+			}
+		}
+	}
+	return (a);
+}
