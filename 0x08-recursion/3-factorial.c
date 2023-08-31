c9d695bf777c2208e17b8a03d0132197eac00507
oluwatimtexx <oluwatimtex667@gmail.com>
2023-08-31 09:15:52 -0700

    recursive function to get the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - Entry point to function that returns factorial of a given number
+ * @n: Number
+ * Return: 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if(n == 0)
+		return (1);
+	else
+		return (factorial(n-1) * n);
+}
